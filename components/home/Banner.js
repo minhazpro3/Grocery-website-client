5c942480611add5fd8b2f6dfd311a424a8e05f8b
minhazpro3 <minhazpro1@gmail.com>
2023-04-07 12:42:11 +0600

    banner section

--- components/home/Banner.js
+++ components/home/Banner.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
 import React from "react";
+import banner from "../../public/banner.jpg";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="relative  w-[100%] h-[300px] md:h-[550px] ">
+        <Image src={banner} alt="banner img" fill />
+      </div>
+      <div className="container mx-auto px-8 md:px-0 relative ">
+        <div className="absolute top-1">
+          <h5>Decorate your home</h5>
+          <h2>Good Healthy</h2>
+          <h5>Food is good choice every day for us.</h5>
+          <h5>And make sure you get enough nutrients every day.</h5>
+          <button>Shop Now</button>
+          <button>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  images: {
+    domains: [
+      "images.pexels.com",
+      "res.cloudinary.com",
+      "animals.png",
+      "i.ibb.co",
+      "img.freepik.com",
+    ],
+  },
+};
