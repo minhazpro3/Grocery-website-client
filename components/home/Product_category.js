663adda19c34250646c7f012b0aaf5058713cd06
minhazpro3 <minhazpro1@gmail.com>
2023-04-07 23:35:46 +0600

    added categories section

--- components/home/Product_category.js
+++ components/home/Product_category.js
@@ -1,6 +1,58 @@
 import React from "react";
 
 const Product_category = () => {
+  const products = [
+    {
+      id: 1,
+      link: "https://i.ibb.co/f4ZNNPJ/download-1.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 2,
+      link: "https://i.ibb.co/VpWqkX2/download-2.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 3,
+      link: "https://i.ibb.co/ZVbZKHY/download-3.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 4,
+      link: "https://i.ibb.co/2hGsMQY/download-4.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 5,
+      link: "https://i.ibb.co/wBnLcN0/download-5.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 6,
+      link: "https://i.ibb.co/TgqkNBk/download.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 7,
+      link: "https://i.ibb.co/W28Wfjn/download.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 8,
+      link: "https://i.ibb.co/276fSVq/images-3.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 9,
+      link: "https://i.ibb.co/HDR27dd/images-4.jpg",
+      title: "cooking chicken",
+    },
+    {
+      id: 10,
+      link: "https://i.ibb.co/5hcmpp9/images.jpg",
+      title: "cooking chicken",
+    },
+  ];
   const prev = () => {
     document.getElementById("slideContent").scrollLeft -= 500;
   };
@@ -10,137 +62,45 @@ const Product_category = () => {
   };
   return (
     <div className="container  mx-auto px-8 md:px-0 my-5">
-      {" "}
-      <div className=" flex justify-end gap-3">
-        <button onClick={prev} className="btn btn-circle">
+      <h4 className="text-center text-4xl font-medium underline ">
+        Top Categories
+      </h4>{" "}
+      <div className=" flex justify-end gap-4">
+        <button
+          onClick={prev}
+          className=" bg-gray-500 rounded-full px-[18px] py-2 text-2xl "
+        >
           ❮
         </button>
-        <button onClick={next} className="btn btn-circle">
+        <button
+          onClick={next}
+          className="bg-gray-500 rounded-full px-[18px] py-2 text-2xl"
+        >
           ❯
         </button>
       </div>
       <div
         id="slideContent"
-        className=" carousel flex items-center gap-4 overflow-x-auto scrollbar-hide  scroll-smooth  my-4"
+        className="  flex items-center gap-4 overflow-x-auto scrollbar-hide scroll-smooth  my-4"
       >
         {/* card 1 */}
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/B2ZcYmb/purepng-1.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Camera</h2>
+        {products.map((product) => (
+          <div key={product.id}>
+            <div className="card w-72 h-60 bg-[#F0F0F0] ">
+              <figure className="px-10 pt-10">
+                <img
+                  style={{ height: "170px" }}
+                  src={product.link}
+                  alt="Shoes"
+                  className="rounded-xl "
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">{product.title}</h2>
+              </div>
             </div>
           </div>
-        </div>
-        {/* card 2 */}
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/q04gYDg/gaming-and-vr-removebg-preview.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Gaming & VR</h2>
-            </div>
-          </div>
-        </div>
-        {/* card 3 */}
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/VjBr0M1/phones-and-tablets-removebg-preview.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Cell phones & tablets</h2>
-            </div>
-          </div>
-        </div>
-        {/* card 4 */}
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/VjBr0M1/phones-and-tablets-removebg-preview.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Cell phones & tablets</h2>
-            </div>
-          </div>
-        </div>
-        {/* card 5 */}
-
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/KNjDHVm/Headphones-Transparent-PNG-1.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Music & Audio</h2>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/KNjDHVm/Headphones-Transparent-PNG-1.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Music & Audio</h2>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/KNjDHVm/Headphones-Transparent-PNG-1.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Music & Audio</h2>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="card w-72 h-80 bg-[#F0F0F0] ">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://i.ibb.co/KNjDHVm/Headphones-Transparent-PNG-1.png"
-                alt="Shoes"
-                className="rounded-xl "
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Music & Audio</h2>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
