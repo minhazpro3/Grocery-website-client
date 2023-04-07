661aea14b180646e88609759322310783f4510c3
minhazpro3 <minhazpro1@gmail.com>
2023-04-07 15:38:37 +0600

    banner styles done

--- components/home/Banner.js
+++ components/home/Banner.js
@@ -4,18 +4,32 @@ import banner from "../../public/banner.jpg";
 
 const Banner = () => {
   return (
-    <div>
+    <div className="relative">
       <div className="relative  w-[100%] h-[300px] md:h-[550px] ">
         <Image src={banner} alt="banner img" fill />
       </div>
-      <div className="container mx-auto px-8 md:px-0 relative ">
-        <div className="absolute top-1">
-          <h5>Decorate your home</h5>
-          <h2>Good Healthy</h2>
-          <h5>Food is good choice every day for us.</h5>
-          <h5>And make sure you get enough nutrients every day.</h5>
-          <button>Shop Now</button>
-          <button>Read more</button>
+      <div className="absolute top-12 md:top-20 left-6 md:left-16 right-6 md:right-16">
+        <div className="">
+          <h5 className="text-xl md:text-2xl text-white ">
+            Decorate your home
+          </h5>
+          <h2 className="text-5xl md:text-8xl text-white my-3 md:my-5">
+            Good Healthy
+          </h2>
+          <h5 className="text-xl md:text-2xl text-white ">
+            Food is good choice every day for us.
+          </h5>
+          <h5 className="text-xl md:text-2xl text-white  md:my-2  ">
+            And make sure you get enough nutrients every day.
+          </h5>
+          <div className="my-3 md:my-6">
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+              Shop Now
+            </button>
+            <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded mx-2">
+              Read more
+            </button>
+          </div>
         </div>
       </div>
     </div>
